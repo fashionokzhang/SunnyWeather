2592a51da40783cec1fb10c86ca0e6f2cc9d5b1a
fashionokzhang <1079429723@qq.com>
2021-06-24 18:24:44 +0800

    Complete

--- app/src/main/java/com/jc/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/jc/sunnyweather/ui/place/PlaceFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jc.sunnyweather.R
 import com.jc.sunnyweather.ui.MainActivity
+import com.jc.sunnyweather.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.fragment_place.*
 
 class PlaceFragment : Fragment() {
 
@@ -55,7 +58,7 @@ class PlaceFragment : Fragment() {
                 adapter.notifyDataSetChanged()
             }
         }
-        viewModel.placeLiveData.observe(this, Observer { result ->
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
             val places = result.getOrNull()
             if (places != null) {
                 recyclerView.visibility = View.VISIBLE
