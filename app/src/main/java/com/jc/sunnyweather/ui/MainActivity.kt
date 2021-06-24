35a9b6da33c120fc76fba5a90433b1550e53bbad
fashionokzhang <1079429723@qq.com>
2021-06-24 18:16:50 +0800

    Complete

---
+++ app/src/main/java/com/jc/sunnyweather/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jc.sunnyweather.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jc.sunnyweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
